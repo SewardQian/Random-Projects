b2a82e70b7810b942e4a96c89c1f70ae5a396535
SewardQian <shujian.qian@mail.utoronto.ca>
2018-08-21 16:18:39 +0800

    adding the header file

--- 08_PairingHeapCpp/PairingHeap.h
+++ 08_PairingHeapCpp/PairingHeap.h
@@ -1,4 +1,51 @@
 #ifndef _pairingHeap_h
 #define _pairingHeap_h
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+struct heapNode{
+	T data;
+	heapNode* left;
+	heapNode* right;
+
+	heapNode(T d, heapNode* l = NULL, heapNode* r = NULL){
+		data = d;
+		left = l;
+		right = r;
+	}
+};
+
+template <typename T>
+class PairingHeap{
+public:
+	PairingHeap();
+	~PairingHeap();
+
+	T findMin() const;
+	void findMin();
+	void insert();
+	void meld();
+	void decreaseKey();
+	void del();
+
+
+private:
+
+	heapNode<T>* root;
+
+};
+
 #endif
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+
