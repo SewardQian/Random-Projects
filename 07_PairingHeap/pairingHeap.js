27f62e04a90ab2f7e1c51566a5aebd48e7f7d61f
SewardQian <shujian.qian@mail.utoronto.ca>
2018-08-21 15:17:44 +0800

    add a pairing heap in cpp

---
+++ 07_PairingHeap/pairingHeap.js
@@ -0,0 +1,37 @@
+class heapNode{
+	constructor(x,left,right){
+		this.item = x;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+class PairingHeap{
+	constructor(){
+
+	}
+
+	findMin(){
+
+	}
+
+	deleteMin(){
+
+	}
+
+	insert(){
+
+	}
+
+	meld(){
+
+	}
+
+	decreaseKey(){
+
+	}
+
+	delete(){
+
+	}
+}
