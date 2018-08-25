cd98c3b58c1cbd1ebffc88474e112c3ca195e662
SewardQian <shujian.qian@mail.utoronto.ca>
2018-08-25 16:42:35 +0800

    writing heap methods

--- 09_HeapCpp/Heap.h
+++ 09_HeapCpp/Heap.h
@@ -15,12 +15,15 @@
 		void insert(T value);
 		T extract();
 
+
 		friend ostream& operator<<(ostream& out,const Heap<T> heap);
 
 	private:
-		int size = 0;
-		int capacity = 100;
-		T array[100];
+		int size;
+		int capacity;
+		T* array;
+		int up(int origin) const;
+		int down(int origin) const;
 
 		void resize();
 
@@ -38,30 +41,73 @@ using namespace std;
 
 template <typename T>
 Heap<T>::Heap(){
-
+	size = 0;
+	capacity = 100;
+	array = malloc(capacity*sizeof(T));
 }
 
 template <typename T>
 Heap<T>::~Heap(){
-
+	free(array);
 }
 
 template <typename T>
 int Heap<T>::getSize() const{
-
+	return size;
 }
 
 template <typename T>
 void Heap<T>::insert(T value){
+	resize();
+	int current = size;
+	
+	size++;
+	//TO DO
+	//
+	
 
 }
 
 template <typename T>
 T Heap<T>::extract(){
+	T returnValue = array[0];
+	//TO DO
 
+	return returnValue;
 }
 
 template <typename T>
-void resize(){
+void Heap<T>::resize(){
+	if (capacity>=size){
+		T* newArray = malloc(2*capacity*sizeof(T));
+		for (int i = 0; i < capacity; i++){
+			newArray[i] = array[i];
+		}
+		capacity*=2;
+		free(array);
+		array = newArray;
+	}
+}
 
+template <typename T>
+int Heap<T>::up(int origin) const{
+	return (int) (origin-1)/2;
+}
+
+template <typename T>
+int Heap<T>::down(int origin) const{
+	return origin*2;
+}
+
+template <typename T>
+ostream& operator<<(ostream& out,const Heap<T> heap){
+	int lastIndex = Heap<T>::size - 1;
+	out<<"["
+	for (int i = 0; i < Heap<T>::size; i++){
+		out<<Heap<T>::array[i];
+		if (i!=lastIndex){
+			out<<",";
+		}
+	}
+	out<<"]";
 }
